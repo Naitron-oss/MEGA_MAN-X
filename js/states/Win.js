fb9a8276e83bd5b3ca935727af025929584aa1bf
Kweena <michael.kweena.fossoul@gmail.com>
2016-04-22 16:43:12 +0200

    Add win text + score

--- js/states/Win.js
+++ js/states/Win.js
@@ -6,10 +6,22 @@ Megaman.Win.prototype = {
 		console.log("Game Over")
 
 
-		var replayButton = this.game.add.button(0, 0, "Win", this.replay, this);
-			// replayButton.anchor.setTo(0.5,0.5);
-			replayButton.scale.x = .4;
-			replayButton.scale.y = .4;
+		var mainScreen = this.game.add.sprite(0,0,"mainScreen");
+			mainScreen.scale.x = .4;
+			mainScreen.scale.y = .4;
+
+		var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 65,"logo");
+			logo.scale.x = .5;
+			logo.scale.y = .5;
+			logo.anchor.set(0.5);
+
+		stateText = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 50,' ', { font: '12px Arial', fill: '#fff' });
+    	stateText.anchor.setTo(0.5, 0.5);
+
+	    this.game.input.onTap.addOnce(this.replay,this);
+
+
+    	stateText.text=" GG \n You Win ! \n Score = 84986 pt ! ";
 		
 	},
 	replay : function(){
