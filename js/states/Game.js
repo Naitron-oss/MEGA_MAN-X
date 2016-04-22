7b409f20c591ddb43374597b2424b15167b2b03b
85m <crush_284@hotmail.com>
2016-04-22 15:42:24 +0200

    maj ennemies 2

--- js/states/Game.js
+++ js/states/Game.js
@@ -1,10 +1,16 @@
 Megaman.Game = function(){}
 
+
+var enemyArray = [];
+var typeArray = [1,2,3];
+
 Megaman.Game.prototype = {
 	create: function(){ 
 		console.log("Game Screen")
 
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 		//crée un joueur
+
 		this.game.player = new Megaman.Player(this.game, "Batman");
 
 		this.game.boss = new Megaman.Boss(this.game, "Boss");
@@ -19,12 +25,19 @@ Megaman.Game.prototype = {
 		this.game.player.bullets.setAll('outOfBoundsKill', true);
 		this.game.player.bullets.setAll('checkWorldBounds', true);
 
+		//this.game.player = new Megaman.Player(this.game, "Batman");
+		//this.game,"nom",type,posx,posy
+		for (var i = 0; i < 10; i++) {
+			var t = typeArray[Math.floor(Math.random()*typeArray.length)];
+			enemyArray.push(new Megaman.Enemy(this.game, "Mario", t, 10*i, 20*i));
+		}
 
 		// bouton a retirer juste pour passer a l'ecran suivant
-		var gameOverButton = this.game.add.button(600, 320, "play", this.stopTheGame, this);
-		gameOverButton.anchor.setTo(0.5,0.5);
-		
-		
+		//var gameOverButton = this.game.add.button(600, 320, "play", this.stopTheGame, this);
+		//gameOverButton.anchor.setTo(0.5,0.5);
+
+		this.map.setCollisionBetween(1, 1);
+
 	},
 	stopTheGame : function(){
 		// tue le joueur
@@ -36,6 +49,10 @@ Megaman.Game.prototype = {
 		/* DEBUG PLAYER */
 		this.game.debug.body(this.game.player);
 		this.game.debug.body(this.game.boss);
+
+
+		//console.log("bouge")
+		//PATTERN DEPLACEMENT
 
 		//mise à jour globale du jeu
 		this.game.player.body.velocity.x = 0;
@@ -68,6 +85,21 @@ Megaman.Game.prototype = {
 			this.game.player.shoot();
 		}
 
+		/* Gestion des ennemies */
+		for (var i = 0; i < enemyArray.length; i++) {
+			this.game.physics.arcade.collide(enemyArray[i], this.layer);
+			switch(enemyArray[i].type){
+		    	case 1:
+		    		enemyArray[i].move();
+		    	break;
+		    	case 2:
+		    		enemyArray[i].shoot();
+		    	break;
+		    	case 3:
+		    		enemyArray[i].fly();
+		    	break;
+		    }
+		}
 	}
 }
 
--- js/states/Preload.js
+++ js/states/Preload.js
@@ -19,6 +19,7 @@ Megaman.Preload.prototype = {
 	    this.game.load.image('bullet','assets/tiles/bullet.png');
 
 	    //spritesheet (key, url, frameWidth, frameHeight, frameMax, margin, spacing
+
 		this.game.load.spritesheet("bossRun","assets/tiles/bossRun.png", 32, 32, 5);
 		this.game.load.spritesheet("bossJump","assets/tiles/bossJump.png", 32, 40, 3);
 		this.game.load.spritesheet("boss","assets/tiles/boss.png", 32, 32, 2);
@@ -26,9 +27,11 @@ Megaman.Preload.prototype = {
 		this.game.load.spritesheet("bossBullets","assets/tiles/bossBullets.png", 16, 32, 3);
 		this.game.load.spritesheet("nom","url_60x80.png", 60, 80, 10);
 		this.game.load.spritesheet('megaman',"assets/tiles/tileMegaman.png",32,32,11);
+
+		//this.game.load.spritesheet("nom","url_60x80.png", 60, 80, 10);
 		// image (key, url)
-		this.game.load.image("nom","url");
-		this.game.load.image("play","url");
+		//this.game.load.image("nom","url");
+		//this.game.load.image("play","url");
 	},
   	create: function(){
   		console.log("Preload finished")
